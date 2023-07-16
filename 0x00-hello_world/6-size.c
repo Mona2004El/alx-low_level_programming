57f6b4894b56003d6a806f3912af1a74a6a1305c
Mona2004El <monmon kwalied@gmail.com>
2023-07-16 13:45:14 -0700

    death

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,6 @@ printf("Size of a int : %u byte(s)\n",  sizeof(int));
 printf("Size of a long int : %u byte(s)\n", sizeof(long int));
 printf("Size of a long long int : %u byte(s)\n", sizeof(long long int));
 printf("Size of a float : %u byte(s)\n", sizeof(float));
-printf("\n");
 
 return (0);
 }
