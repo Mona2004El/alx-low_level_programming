2cc1c9414dee4508fae2221176234f80b50afcab
Mona2004El <monmon kwalied@gmail.com>
2023-07-16 13:37:55 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,12 +5,14 @@
  *description: Size is not grandeur, and territory does not make a nation
  *Return: always 0
  */
-void main(void)
+int main(void)
 {
 printf("Size of a char : %u byte(s)\n", sizeof(char));
 printf("Size of a int : %u byte(s)\n",  sizeof(int));
 printf("Size of a long int : %u byte(s)\n", sizeof(long int));
 printf("Size of a long long int : %u byte(s)\n", sizeof(long long int));
 printf("Size of a float : %u byte(s)\n", sizeof(float));
-return;
+printf("\n");
+
+return (0);
 }
