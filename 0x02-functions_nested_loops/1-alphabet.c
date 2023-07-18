f42004a17549edb2eac79cc55ff2c2c7bf71a72b
Mona2004El <monmon kwalied@gmail.com>
2023-07-18 04:00:08 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 
 /**
  *print_alphabet - function that prints the alphabet
- *Return: Always 0.
+*
 */
 
 void print_alphabet(void)
@@ -10,9 +10,6 @@ void print_alphabet(void)
 	int ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-	{
 	_putchar(ch);
-	}
 	_putchar('\n');
-	return (0);
 }
