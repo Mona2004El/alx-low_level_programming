79c2e0d78e64e7215d10ae780b91f73384dc456e
Mona2004El <monmon kwalied@gmail.com>
2023-07-18 03:51:06 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_alphabet - function that prints the alphabet
+ *Return: Always 0.
+*/
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	_putchar(ch);
+	}
+	_putchar('\n');
+	return (0);
+}
