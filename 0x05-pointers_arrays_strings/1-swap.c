ce7d6a88f7fdaae258e635ea395e789014119a0f
Mona2004El <monmon kwalied@gmail.com>
2023-07-24 05:18:55 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/*
+ *swap_int - swaps the values of two integers
+ *@a: first int
+ *@b: second int
+ *
+ * Return: always 0
+ */
+void swap_int(int *a, int *b)
+{
+	int num;
+	num = *a;
+	*a = *b;
+	*b = num;
+}
