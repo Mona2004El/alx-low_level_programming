1d0c2a26ce9d0a031ddcfbad5633d4a2b9a540db
Mona2004El <monmon kwalied@gmail.com>
2023-08-01 13:50:59 -0700

    new task

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ **_memset _ fills memory with a constant byte.
+ *@s : string input
+ *@b : character input
+ *@n : unsigned integer input
+ *Return : always 0
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int num;
+
+	for (num = 0; n > 0; num++, n--)
+	{
+		s[num] = b;
+	}
+	return (s);
+}
