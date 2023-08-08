b5bf8dbed936294eaa22f2c0039af57e5b876d70
Mona2004El <monmon kwalied@gmail.com>
2023-08-08 06:29:36 -0700

    malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ **create_array - creates an array of chars,
+ *and initializes it with a specific char.`
+ *
+ *@c: string
+ *@size: number of bytes
+ *
+ *Return: always 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr = malloc(size);
+
+	if (size == 0 || ptr == 0)
+		return (null);
+
+	while (size--)
+		ptr[size] = c;
+
+	return (ptr);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ **_strdup - returns a pointer to a newly allocated space in memory,
+ *which contains a copy of the string given as a parameter.
+ *@str: char data type string
+ *
+ *Return: always 0
+ */
+char *_strdup(char *str)
+{
+	if (str == NULL)
+		return (NULL);
+
+	for (size = 0; str[size] != '\0'; size++)
+		;
+
+	char *ptr = malloc(size * sizeof(*str) + 1);
+
+	if (ptr == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		ptr[i] = str[i];
+	}
+	return (ptr);
+}
