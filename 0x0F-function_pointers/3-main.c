b3f67327432daec8c5c4c66222228eadcec93a2c
Mona2004El <monmon kwalied@gmail.com>
2023-08-16 09:06:41 -0700

    pointer

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+/**
+ *main - check the code
+ *@argc: count of arguments
+ *@argv: value of arguments
+ *
+ *Return: success at 0
+ */
+int main(int argc, char **argv)
+{
+	int (*op_func)(int, int), a, b;
+
+	if (argc != 4)
+	{
+	
+		printf("error\n"), exit(98);
+	}
+	if (argv[2][1])
+	{
+		printf("Error\n"), exit(99);
+	}
+
+		op_func = get_op_func(argv[2]);
+
+		if (op_func == NULL)
+		{
+			printf("error\n"), exit(99);
+		}
+
+		a = atoi(argv[1]);
+		b = atoi(argv[3]);
+
+		printf("%d\n", op_func(a, b));
+		return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include "3-calc.h"
+/**
+ * op_add - operation of sum
+ *@a: 1st int
+ *@b: 2nd int
+ *
+ *Return: int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - operation of substract
+ *@a: 1st int
+ *@b: 2nd int
+ *Return: int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - operation of multibicate
+ *@a: 1st int
+ *@b: 2nd int
+ *Return: int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - operation of division
+ *@a: 1st int
+ *@b: 2nd int
+ *Return: int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - operation of mode
+ *@a: 1st int
+ *@b: 2nd int
+ *Return: int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
