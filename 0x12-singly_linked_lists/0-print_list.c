93705513c3dfad8b2096f381b0bfabfcbc60dc26
Mona2004El <monmon kwalied@gmail.com>
2023-08-24 15:24:07 -0700

    linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,4 @@
-#include "list.h"
+#include "lists.h"
 
 /**
  *_strlen - returns the length
@@ -8,11 +8,12 @@
  */
 int _strlen(char *s)
 {
-	int i;
+	int i = 0;
 
 	if (!s)
 		return (0);
-	for (i = 0; *s++; i++)
+	while (*s++);
+	i++;
 		return (i);
 
 }
