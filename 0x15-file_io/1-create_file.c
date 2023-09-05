a3b5dc0d0a01232819200caa2dec67346a3a4b9a
Mona2004El <monmon kwalied@gmail.com>
2023-09-05 11:48:11 -0700

    new

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ *_strlen - returns the length of a string
+ *@s: string
+ *
+ *Return: always 0
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+#include "main.h"
+
+/**
+ * create_file - creates a file and fills it with text
+ * @filename: name of the file to create
+ * @text_content: text to write in the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes = 0, len = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (len)
+		bytes = write(fd, text_content, len);
+	close(fd);
+	return (bytes == len ? 1 : -1);
+}
